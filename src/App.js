7fa9c93ac19472250192865f617965647f68f406
unknown <sm668830@gmail.com>
2022-05-17 03:05:25 +0200

    lab1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Task1 from "./Components/Task1";
+import Task2 from "./Components/Task2";
+
+let App= ()=>{
+    return(
+        <div>
+            <Task1/>
+            <br></br>
+            <Task2/>
+
+        </div>
+    )
+}
+
+export default App;
